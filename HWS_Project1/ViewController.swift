b84b4bd1372b3af8c95bbae0514cfd000ff5f963
nofprog <k.jin3217@gmail.com>
2021-02-26 18:46:53 +0900

    initial commit

--- HWS_Project1/ViewController.swift
+++ HWS_Project1/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictuers = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath! //アプリに追加された画像がある場所
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("0") {
+                pictuers.append(item)
+            }
+        }
+        
+        print(pictuers)
+        //["03.webp", "02.webp", "09.webp", "05.webp", "04.webp", "08.webp", "07.webp", "06.webp", "01.webp"]
+
+        
     }
 
 
